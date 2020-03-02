21ae2739c3aee84b556512c13c0191dfe927b63a
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-03-02 10:41:31 +0100

    Improve example.py and inline docs for poet.py

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
-import os
-import genius
-import poet
+from genius import Genius
+from poet import Poet
 
-g = genius.Genius()
+g = Genius()
 artists = ['Bob Dylan', 'Tim Buckley', 'The Beatles']
 text = g.get_artists_lyrics(artists, songs_per_artist=15)
 
-p = poet.Poet()
+p = Poet()
 p.train_on(text, n_epochs=30)
 poem = p.generate(u'Hey ', n_gen_chars=2000)
 print(poem)
--- poet.py
+++ poet.py
@@ -1,4 +1,4 @@
-import os
+import os, warnings
 from time import time
 import numpy as np
 import tensorflow as tf
@@ -7,6 +7,10 @@ import tensorflow as tf
 
 class Poet:
     """An artificial poet.
+
+    A Poet object is an object that wraps a recurrent predictive TensorFlow
+    model, and can learn how to predict the next character in a sequence of
+    characters extracted from a possibly large text corpus.
     """
 
     def __init__(self, name: str = None, embedding_dim: int = 256, rnn_units: int = 1024):
@@ -14,6 +18,10 @@ class Poet:
 
         Create a Poet, optionally specifying its name, embedding
         dimensionality, and number of recurrent hidden units.
+
+        :param name: string that uniquely identifies the poet
+        :param embedding_dim: output dimensionality of the hidden layer
+        :param rnn_units: number of units in the hidden layer
         """
 
         self.name = name or str(time()).replace('.', '')
@@ -22,10 +30,12 @@ class Poet:
         self.rnn_units = rnn_units
         self.model = None
         self.weights = None
-        self.checkpoint_dir = './training_checkpoints' + '_' + self.name
+        self.checkpoint_dir = None
 
     def build_model(self, batch_size: int = 1) -> None:
         """Build the Poet's internal model of the world.
+
+        :param batch_size: the number of inputs to be fed at once to the model
         """
 
         vocab_size = len(self.vocab)
@@ -39,8 +49,22 @@ class Poet:
             tf.keras.layers.Dense(vocab_size)
             ])
 
-    def preprocess(self, text: str, batch_size: int) -> tf.data.Dataset:
+    def preprocess(self, text: str, batch_size: int = 32) -> tf.data.Dataset:
         """Preprocess a text corpus for supervised learning.
+
+        Given a UTF-8 text string, map each possible sequence of 100 contiguous
+        characters to the sequence made of the successors of each of these
+        characters (where the "successor" of a character is the character that
+        comes after it in the text). Optionally provide a batch size that will
+        be used when training the model to predict the next character in the
+        text corpus.
+
+        This method returns a tf.data.Dataset that relates each character to
+        its successor, in "windows" of 100 characters at a time.
+
+        :param text: a text corpus made of UTF-8 characters
+        :param batch_size: the number of sequences fed at once to the model
+        :return: a tf.data.Dataset that relates each character to its successor
         """
 
         # create mappings from unique characters to indices, and viceversa:
@@ -48,45 +72,55 @@ class Poet:
         self.char2idx = {u: i for i, u in enumerate(self.vocab)}
         self.idx2char = np.array(self.vocab)
 
-        # create training examples & targets:
+        # sequences of characters are just batches of characters:
         text_as_int = np.array([self.char2idx[c] for c in text])
         char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
-
-        # sequences of characters are just batches:
         max_seq_length = 100 # max length of a sentence for a single input
         sequences = char_dataset.batch(max_seq_length + 1, drop_remainder=True)
 
-        # from each sequence, form the input and target text:
-        split_input_target = lambda chunk: (chunk[:-1], chunk[1:])
-        dataset = sequences.map(split_input_target)
+        # associate training features to labels:
+        split_input_label = lambda chunk: (chunk[:-1], chunk[1:])
+        dataset = sequences.map(split_input_label)
 
-        # shuffle the sequences of characters, and create bacthes of them:
+        # shuffle the sequences (batches) of characters:
         dataset = dataset.shuffle(len(text))
+
+        # create bacthes of sequences ("batches of batches"):
         dataset = dataset.batch(batch_size, drop_remainder=True)
 
         return dataset
 
     def train_on(self, text: str, n_epochs: int = 1, checkpoints: bool = False) -> None:
         """Train the poet's internal model on a text corpus.
+
+        Train the poet's internal model of the world (with gradient-based
+        optimitazion) on a UTF-8 text corpus, for a specified number of
+        epochs, with or without regularly saving checkpoints of the model's
+        parameters.
+
+        :param text: a text corpus made of UTF-8 characters
+        :param n_epochs: the number of epochs to train the model
+        :param checkpoints: whether to save the weighs on disk after each epoch
         """
 
-        # preprocess the data and set a couple of other things:
+        # preprocess the text corpus:
         training_batch_size = 64
         dataset = self.preprocess(text, training_batch_size)
 
-        # build model with adequate training batch_size:
+        # build a TensorFlow model with adequate training batch size:
         self.build_model(batch_size=training_batch_size)
 
         callbacks = []
         if checkpoints:
             # ensure that checkpoints are saved during training:
+            self.checkpoint_dir = './checkpoints' + '_' + self.name
             ckpt_prefix = os.path.join(self.checkpoint_dir, 'ckpt_{epoch}')
             checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
                 filepath=ckpt_prefix,
                 save_weights_only=True)
             callbacks.append(checkpoint_callback)
 
-        # set various optimization hyper-parameters:
+        # set some optimization hyper-parameters:
         loss = lambda t, y: tf.keras.losses.sparse_categorical_crossentropy(t, y, from_logits=True)
         self.model.compile(
             optimizer='adam',
@@ -97,17 +131,37 @@ class Poet:
             epochs=n_epochs,
             callbacks=callbacks)
 
+        # save the weights just learned by the model:
         self.weights = self.model.weights
 
-    def restore(self):
+    def restore(self) -> None:
         """Restore the state of the Poet's model from the latest checkpoint.
+
+        If checkpoints have been saved during training, set the Poet's model
+        parameters (weights) to the state of the latest checkpoint taken.
+
+        :raises: ResourceWarning
         """
 
-        latest_state = tf.train.latest_checkpoint(self.checkpoint_dir)
-        self.model.load_weights(latest_state)
+        if self.checkpoint_dir is None:
+            warnings.warn('No checkpoints have been saved for this poet.',
+                warnings.ResourceWarning)
+        else:
+            latest_state = tf.train.latest_checkpoint(self.checkpoint_dir)
+            self.model.load_weights(latest_state)
 
     def generate(self, start_string: str, n_gen_chars: int = 1000, temperature: float = 1.0) -> str:
         """Generate text using the poet's internal model.
+
+        Generate a specified number of characters by feeding an initial string
+        to the model and using this string as a starting point to sample a
+        character that is likely to appear after the previously generated
+        character.
+
+        :param start_string: the initial string fed to the model
+        :param n_gen_chars: the number of characters to generate
+        :param temperature: how surprising vs. predictable characters should be
+        :return: a new text
         """
 
         # build model with unit batch_size:
@@ -116,26 +170,26 @@ class Poet:
         # restore the model parameters:
         self.model.set_weights([w.numpy() for w in self.weights])
 
-        # encode the starting string:
-        input_eval = [self.char2idx[s] for s in start_string]
-        input_eval = tf.expand_dims(input_eval, 0)
+        # encode the starting string to numbers:
+        model_input = [self.char2idx[s] for s in start_string]
+        model_input = tf.expand_dims(model_input, 0)
 
-        generated_text = []
+        model_output = []
         self.model.reset_states()
         for c in range(n_gen_chars):
 
-            predictions = self.model(input_eval)
+            predictions = self.model(model_input)
 
             # remove the batch dimension:
             predictions = tf.squeeze(predictions, 0)
 
-            # use a categorical distribution to sample the next character:
+            # sample the next character:
             predictions = predictions / temperature
-            predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+            predicted_id = tf.random.categorical(predictions, 1)[-1, 0].numpy()
+            model_output.append(self.idx2char[predicted_id])
 
             # pass the predicted character as the next input to the model,
             # along with the previous hidden state:
-            input_eval = tf.expand_dims([predicted_id], 0)
-            generated_text.append(self.idx2char[predicted_id])
+            model_input = tf.expand_dims([predicted_id], 0)
 
-        return start_string + ''.join(generated_text)
+        return start_string + ''.join(model_output)
