317c62527fe210f6f01419fa2ae9ce7afe670ed1
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-03-03 11:58:41 +0100

    Improve example.py

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ artists = ['Bob Dylan', 'Tim Buckley', 'The Beatles']
 text = g.get_artists_lyrics(artists, per_artist=15)
 
 p = Poet()
-p.train_on(text, n_epochs=30)
-poem = p.generate('Hey ', n_gen_chars=2000)
+p.train_on(text, n_epochs=10)
+poem = p.generate('Hey ', n_gen_chars=1000)
 print(poem)
